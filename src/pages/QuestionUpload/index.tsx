4327efac7bf04bd6117ae0cacc37d82b1e2d1cb0
davidyfxu <82054510@qq.com>
2023-08-30 07:55:42 +0800

    feat: solveGPT commit

--- src/pages/QuestionUpload/index.tsx
+++ src/pages/QuestionUpload/index.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Button, Upload } from "@douyinfe/semi-ui";
+import { IconUpload } from "@douyinfe/semi-icons";
 
 const QuestionUpload = () => {
-  return <div>QuestionUpload</div>;
+  return (
+    <div>
+      <input type="file" onChange={(e) => console.log(e)} />
+      <Upload
+        listType="picture"
+        headers={{
+          authorization: "authorization-text",
+        }}
+        beforeUpload={async (file) => {
+          try {
+            const API_BASE_URL = "http://118.89.117.111";
+            const formData = new FormData();
+            formData.append("image", file.file.fileInstance);
+            console.log("file", file);
+            const res = await fetch(API_BASE_URL + "/submitImage", {
+              method: "POST",
+              body: formData,
+            });
+            console.log(res);
+            const result = await res.json();
+            console.log(result);
+          } catch (e) {
+            console.error("customRequest", e);
+          }
+        }}
+      ></Upload>
+    </div>
+  );
 };
 
 export default QuestionUpload;
